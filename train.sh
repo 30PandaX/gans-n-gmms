b1f70b371cd24fc9ced65cc3a94732cd8bf92fec
Zihan Tan <zt246@g2-login-01.coecis.cornell.edu>
2021-09-09 01:25:30 -0400

    vectorize fmri

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@
 source activate braindecoding
 
 cd /home/zt246/storage/celeba/gans-n-gmms
-python -u mfa_train_celeba.py --num_components 10 --samples_per_sub_component 10
+python -u mfa_train_celeba.py --num_components 75 --samples_per_sub_component 10
 
